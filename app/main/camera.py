ea71299638c90cf95635fffddfc035b3369c239b
Raspberry Main <j.m.koelewijn@viinum.com>
2021-09-13 15:10:34 +0200

    Camera bug 3

--- app/main/camera.py
+++ app/main/camera.py
@@ -5,10 +5,13 @@ def get_picture():
     camera = PiCamera()
     camera.rotation = 180
     camera.resolution = (2592, 1944)
-    camera.start_preview()
-    sleep(5)
-    camera.capture('/var/www/html/gyllcare/app/static/Resources/img/fishlens.jpg')
-    # camera.capture('/home/pi/Viinum/gyllcare/app/static/Resources/img/fishlens.jpg')
-    camera.stop_preview()
-    camera.close()
-    print("Does camera.close work??")
+    try:
+        camera.start_preview()
+        sleep(5)
+        camera.capture('/var/www/html/gyllcare/app/static/Resources/img/fishlens.jpg')
+        # camera.capture('/home/pi/Viinum/gyllcare/app/static/Resources/img/fishlens.jpg')
+        camera.stop_preview()
+        print("Madness 1")
+    finally:
+        camera.close()
+        print("maddnes 2")
